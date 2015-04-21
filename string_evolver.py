bc2d3bc05b69d833085c230792005d889043292d
F. Gau <fgau@pyneo.org>
2015-04-21 22:20:24 +0200

    fix traits dicts and remove whitespaces

--- string_evolver.py
+++ string_evolver.py
@@ -3,16 +3,17 @@ from libs.individuals import Individual
 from libs.biosphere import Biosphere
 
 TARGET = raw_input("String to evolve: ");
+traits = {}
 
 class GuesserTrait(IntegerTrait):
-    int_min = 0 
-    int_max = 255 
-    delta = 5 
+    int_min = 0
+    int_max = 255
+    delta = 5
 
 class StringGuesser(Individual):
     traits = traits
     mutation_chance = 0.15
-    single_trait_mutation = True 
+    single_trait_mutation = True
 
     def __repr__(self):
         chars = []
@@ -30,29 +31,28 @@ class StringGuesser(Individual):
             x1 = self[str(i)]
             diffs += (x1 - x0) ** 2
 
-        return diffs 
+        return diffs
 
 class GuesserBiosphere(Biosphere):
     species = StringGuesser
     specimens = 100
     nfittest = 15
     new_children = 50
-    survival = 3 
+    survival = 3
 
 
-traits = {}
 for c in range(len(TARGET)):
     traits[str(c)] = GuesserTrait
 
 def main():
     biosphere = GuesserBiosphere()
-    
+
     i = 0
     while True:
         best = biosphere.best()
         print "Generation %d: %s   Best = %d" % (
             i, best, best.fitness())
-        
+
         if best.fitness() <= 0:
             print "Woot! Got it!"
             break
